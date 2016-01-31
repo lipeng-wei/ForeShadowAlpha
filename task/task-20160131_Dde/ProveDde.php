18bfab4ba2015c47754be0e9263249309ee94676
lipeng_wei <lipeng_wei@hotmail.com>
2016-02-01 06:39:27 +0800

    添加新任务
    //* * * * * * * * * * * * * * *//
    ForeShadow Alpha 第一个可用的版本
    1，整体框架 done
    2，抓取脚本 done
    3，任务框架 done
    4，数据模型 done
    5，前端展示 done
    
    下一步：
    1，升级抓取脚本
    2，添加一些新的策略
    3，自选股等小功能慢慢完善

---
+++ task/task-20160131_Dde/ProveDde.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lipeng_wei
+ * Date: 16-01-31
+ * Time: 上午3:38
+ *
+ * DDE的指标研究 1，飘红数量 2，绝对值的量化
+ *
+ */
+
+
+require_once(dirname(__FILE__). '/../../Require.php');
+require_once(MODULE_PATH . 'DdeData.class.php');
+
+
+class ProveDde extends Task{
+
+    public static $thisTaskBasePath;
+    public static $thisTaskDataPath;
+    public static $thisTaskLogPath;
+    public static $thisTaskTmpPath;
+
+    public static function run(){
+        self::$thisTaskBasePath = dirname(__FILE__). '/';
+        self::$thisTaskDataPath = self::$thisTaskBasePath. 'data/';
+        self::$thisTaskLogPath = self::$thisTaskBasePath. 'log/';
+        self::$thisTaskTmpPath = self::$thisTaskBasePath. 'tmp/';
+
+        self::setNohup(true);
+        //以上为设置
+
+        //以下为具体的函数调用
+
+        self::calcDdeNum('2016-01-04', '2016-01-29');
+
+    }
+
+
+    //统计DDX数量 形成信息表Table
+    public static function calcDdeNum($start, $end){
+        $resultFile = self::$thisTaskDataPath. $start. '_'. $end. '_DdeNum.Table.txt';
+
+        $i = 2;
+
+        $stockList = Refer::getStock();
+        $stockData = array();
+        foreach($stockList as $stkL){
+
+            //if (--$i < 0 ) break;
+
+            $stkT = array(
+                'name' => '',
+                'code' => '',
+                'DdxNum' => 0,
+                'DdxPer' => 0,
+                'DdzNum' => 0,
+                'DdzPer' => 0,
+                'TddzNum' => 0,
+                'TddzPer' => 0,
+                'TotalNum' => 0,
+                'TotalPer' => 0,
+                'DayNum' => 0
+            );
+
+            $stkT['name'] = '<a target="_blank" href="'.Url::getBasePhp('Chart') . '?code=' . $stkL['code']. '">' .
+                '<span class="fore-stock-name">' . $stkL['name'] . '</span>' .
+                '<span class="fore-stock-spell displaynone">' . $stkL['spell'] . '</span></a>';
+            $stkT['code'] =  '<span class="fore-stock-code">' . $stkL['code'] . '</span>';
+
+            $dd = new DdeData($stkL['code']);
+            if (! $dd->prepareData()) continue;
+            $dde = $dd->getDayPeriod($start, $end, 7);
+            //var_dump($dde);
+            if (! $dde) continue;
+
+            foreach($dde as $d){
+                //echo $d['ddx']. '(';
+                //echo ($d['ddx'] > 0). ')';
+                if ($d['ddx'] > 0) $stkT['DdxNum']++;
+                if ($d['ddz'] > 0) $stkT['DdzNum']++;
+                if ($d['tddz'] > 0) $stkT['TddzNum']++;
+            }
+            $stkT['DayNum'] = sizeof($dde);
+            $stkT['DdxPer'] = ceil($stkT['DdxNum'] / $stkT['DayNum'] * 10000) / 10000 ;
+            $stkT['DdzPer'] = ceil($stkT['DdzNum'] / $stkT['DayNum'] * 10000) / 10000 ;
+            $stkT['TddzPer'] = ceil($stkT['TddzNum'] / $stkT['DayNum'] * 10000) / 10000 ;
+
+            $stkT['TotalNum'] = $stkT['DdxNum'] + $stkT['TddzNum'] + $stkT['TddzNum'];
+            $stkT['TotalPer'] = ceil($stkT['TotalNum'] / $stkT['DayNum'] / 3 * 10000) / 10000 ;
+
+            //var_dump($stkT);
+            $stockData[] = $stkT;
+        }
+        parent::putTable($resultFile, $stockData);
+
+    }
+
+
+}
+ProveDde::run();
+
+
+
